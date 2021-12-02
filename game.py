1653dd51551a34871affaa566157e1a392e51478
RyanCqc <minryanyen@yahoo.com>
2021-12-01 20:50:16 -0500

    updated README and game.py

--- game.py
+++ game.py
@@ -5,6 +5,7 @@
 import numpy as np
 from cards import Card
 from shoe import Shoe
+import sys
 
 # global variables
 player = 0
@@ -37,12 +38,6 @@ def draw_cards(player_hand, dealer_hand, shoe) -> tuple:
     return (player_hand, dealer_hand, shoe)
 
 
-def draw_one_card(player_hand, dealer_hand, shoe) -> tuple:
-    player_hand.append(shoe.draw())
-
-    return (player_hand, dealer_hand, shoe)
-
-
 def compare_hands(player_hand, dealer_hand) -> int:
     if score(player_hand) > score(dealer_hand):
         return 1
@@ -69,7 +64,78 @@ def score(hand: Card) -> int:
     return sum
 
 
+def FiftyFive(player_score, dealer_score):
+
+    if player_score == 55 and dealer_score == 55:
+        print("Tie")
+        sys.exit(0)
+    elif player_score == 55:
+        print("Player wins")
+        sys.exit(0)
+    elif dealer_score == 55:
+        print("Dealer wins")
+        sys.exit(0)
+    else:
+        return False
+
+
+def dealer_turn(player_hand, dealer_hand, shoe) -> tuple:
+
+    while score(dealer_hand) < score(player_hand):
+        dealer_hand.append(shoe.draw())
+        print("Dealer hand: " + show_hand(dealer_hand))
+        if score(dealer_hand) > 50:
+            break
+
+    dealer_busted = busted(dealer_hand)
+    if(dealer_busted):
+        print("Dealer busted, player wins")
+        return (True, (player_hand, dealer_hand, shoe))
+
+    return (False, (player_hand, dealer_hand, shoe))
+
+
+def play_turn(player_hand, dealer_hand, shoe) -> tuple:
+
+    print("Hit or stay?")
+    choice = input()
+    player_busted = busted(player_hand)
+
+    while (player_busted == False and choice == "hit"):
+        player_hand.append(shoe.draw())
+        print("Player hand: " + show_hand(player_hand))
+        print("Player score: " + str(score(player_hand)))
+        player_busted = busted(player_hand)
+        if(player_busted):
+            print("Player busted, dealer wins")
+            return (True, (player_hand, dealer_hand, shoe))
+        print("Hit or stay?")
+        choice = input()
+
+    if choice == "stay":
+        while score(dealer_hand) < score(player_hand):
+            dealer_turn(player_hand, dealer_hand, shoe)
+
+        if (compare_hands(player_hand, dealer_hand)) == 1 and player_busted == False:
+            print("Player wins!")
+        elif (compare_hands(player_hand, dealer_hand)) == -1 and dealer_busted == False:
+            print("Dealer wins!")
+        elif(compare_hands(player_hand, dealer_hand) == 0):
+            print("Draw!")
+    return player_hand, dealer_hand, shoe
+
+
+def busted(hand):
+    if score(hand) > 55:
+        return True
+    else:
+        return False
+
+
 if __name__ == '__main__':
+
+    player_busted = False
+    dealer_busted = False
 
     game_state = initial_state()
     game_state = draw_cards(game_state[0], game_state[1], game_state[2])
@@ -85,34 +151,14 @@ if __name__ == '__main__':
     player_score = score(player_hand)
     dealer_score = score(dealer_hand)
 
-    while player_score < 55 and dealer_score < 55:
-        print("Player score: " + str(player_score))
-        print("Dealer score: " + str(dealer_score))
-        print("Remaining cards: " + str(remaining_cards.count()))
-        print("Hit or stay?")
-        choice = input()
+    # evaluate the first five cards
+    FiftyFive(player_score, dealer_score)
 
-        if choice == "hit":
-            game_state = draw_one_card(
-                game_state[0], game_state[1], game_state[2])
-            player_hand = game_state[0]
-            print("Player hand: " + show_hand(player_hand))
-            dealer_hand = game_state[1]
-            remaining_cards = game_state[2]
-            player_score = score(player_hand)
-            dealer_score = score(dealer_hand)
-        else:
-            # dealer will stand on 50
-            # yet to be implemented
+    print("Player score: " + str(player_score))
+    print("Dealer score: " + str(dealer_score))
+    print("Remaining cards: " + str(remaining_cards.count()))
 
-            print("Dealer hand: " + show_hand(dealer_hand))
+    game_over = play_turn(game_state[0], game_state[1], game_state[2])[0]
 
-            if (compare_hands(player_hand, dealer_hand)) == 1:
-                print("Player wins!")
-                break
-            elif (compare_hands(player_hand, dealer_hand)) == -1:
-                print("Dealer wins!")
-                break
-            else:
-                print("Draw!")
-                break
+    print("Player score: " + str(score(player_hand)))
+    print("Dealer score: " + str(score(dealer_hand)))
